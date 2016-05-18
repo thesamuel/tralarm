c0fb6dcce52cac45f992c59a6ff01a812e230b5b
Sam <sam@samgehman.com>
2016-05-17 22:43:16 -0700

    Initial Commit

---
+++ Tralarm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tralarm
+//
+//  Created by Sam Gehman on 5/17/16.
+//  Copyright © 2016 Sam Gehman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tralarm/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tralarm
+//
+//  Created by Sam Gehman on 5/17/16.
+//  Copyright © 2016 Sam Gehman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
